3bba0c9f61a7c2b1038f2f4bcbd3b6433f3ab115
Sadiya <shmiah15@gmail.com>
2025-10-19 18:35:07 +0100

    update

---
+++ 02.Movement/sketch.js
@@ -0,0 +1,22 @@
+let x = 0;
+let y = 0;
+
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  //background(220);
+  //circle 
+  circle(100,30,100)
+
+  //Draw a line
+  let x1 = random(width);
+  let y1 = random(height);
+  //let x1 = mouseX;
+  //let x1 = mouseX;
+  let x2 = random(width)
+  
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+let home = 55;
+let incre = 1;
+
+function setup() {
+  createCanvas(400, 400);
+  console, log(home)
+}
+
+function draw() {
+  background(220);
+  //circle(50,50,50)
+  circle(50,50,25)
+  incre = incre =1;
+  console. log(incre)
+}
