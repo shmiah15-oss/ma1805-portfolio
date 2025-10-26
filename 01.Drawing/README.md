729a291eb844f53bb0a77b3554389cdd82e344b2
Sadiya <shmiah15@gmail.com>
2025-10-26 23:08:34 +0000

    update cats

--- 01.Drawing/README.md
+++ 01.Drawing/README.md
@@ -1,5 +1,11 @@
-Decided to make it cat themed, a reference to both Halloween and the first realsitic computer animated animal in 1968 - Kitty
+Decided to make it cat themed, a inspired by both Halloween and the first realsitic computer animated animal in 1968 - Kitty (you said to have some artistic, cultural and technical - wasn't too sure what that meant)
 
-
-Doing the code for the triangles is difficult - took me quite a while to do, i was going to do multiple cats but decided not to after fiddling with the triangles
+Doing the code for the triangles was difficult - took me quite a while to do, i was going to do multiple cats but decided not to after fiddling with them
 Is there a way to make the coners of the triangle curved?
+(I got the hang of it now - I think)
+
+comments are really convieniant for organising
+
+The background felt to plain as a soilid colour, but i remembered seeing a project with a rainbow background using beginContour
+
+I'm to much of a perfectionist for this
--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -5,6 +5,99 @@ function setup() {
 function draw() {
   background(200, 0, 300);
 
+
+
+  //background detail
+  fill(150, 0, 300)
+
+  //one
+  beginShape();
+  
+  vertex(10, 10);
+  vertex(490, 10);
+  vertex(490, 490);
+  vertex(10, 490);
+
+  beginContour();
+  vertex(30, 30);
+  vertex(30, 470);
+  vertex(470, 470);
+  vertex(470, 30);
+  endContour();
+
+  endShape(CLOSE);
+
+
+  //two
+  beginShape(); 
+
+  vertex(40, 40);
+  vertex(460, 40);
+  vertex(460, 460);
+  vertex(40, 460);
+
+  beginContour();
+  vertex(60, 60);
+  vertex(60, 440);
+  vertex(440, 440);
+  vertex(440, 60);
+  endContour();
+
+  endShape(CLOSE);
+
+
+  //three
+  beginShape(); 
+
+  vertex(70, 70);
+  vertex(430, 70);
+  vertex(430, 430);
+  vertex(70, 430);
+
+  beginContour();
+  vertex(90, 90);
+  vertex(90, 410);
+  vertex(410, 410);
+  vertex(410, 90);
+  endContour();
+
+  endShape(CLOSE);
+
+
+
+  //four
+  beginShape(); 
+
+  vertex(100, 100);
+  vertex(400, 100);
+  vertex(400, 400);
+  vertex(100, 400);
+
+  beginContour();
+  vertex(120, 120);
+  vertex(120, 380);
+  vertex(380, 380);
+  vertex(380, 120);
+  endContour();
+
+  endShape(CLOSE);
+
+
+
+
+
+
+  // the left whiskers
+  line(100, 200, 259, 334);
+  line(100, 250, 279, 280);
+  line(100, 300, 210, 280);
+
+  // the right whiskers
+  line(400, 200, 259, 334);
+  line(400, 250, 279, 280);
+  line(400, 300, 210, 280);
+
+
   // the ears and head
   fill(10);
   triangle(160, 220, 205, 165, 125, 140);
@@ -79,7 +172,7 @@ function draw() {
 
   
   // the eyelids
-  fill(0)
+  fill(0);
   ellipse(300, 224, 35, 7);
   ellipse(205, 224, 35, 7);
 
@@ -89,15 +182,12 @@ function draw() {
   triangle(250, 270, 240, 250, 260, 250);
 
   //the hat
-  fill(0)
+  fill(0);
   triangle(200, 140, 300, 140, 250, 50);
   rect(190, 135, 120, 10, 20);
 
 
-  // the left whiskers
-  line(300, 200, 100, 375);
-  line(330, 300, 100, 300);
-  line(360, 350, 100, 239);
+
 
 
 
