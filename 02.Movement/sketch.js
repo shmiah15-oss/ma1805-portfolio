e43b1a2a590c2dbf498a27ee5ed34a69059f16ce
Sadiya <shmiah15@gmail.com>
2025-10-28 09:57:55 +0000

    updates

--- 02.Movement/sketch.js
+++ 02.Movement/sketch.js
@@ -11,17 +11,30 @@ function draw() {
 
   // background kind of
   noStroke();
-  fill(mouseY, 50);
-  circle(mouseX, mouseY, 100); 
+  fill(mouseY, 255);
+  circle(mouseX, mouseY, 30); 
 
   //x
-  stroke(20)
+  stroke(50);
   fill(255, 128, 128);
   x +=1;
-  circle(x, 50, 20);
-  circle(50, x, 20);
-  circle(50, 20, x);
 
+  //small pink circle going left to right (top)
+  circle(x, 50, 20);
+
+  //small pink circle going down
+  circle(50, x, 20);
+
+
+  //small purple circle going down
+  fill(150, 0, 220);
+  circle(450, x, 20);
+
+  //small purple circle form left to right (bottom)
+  fill(150, 0, 220);
+  circle(x, 450, 20);
+
+  //down
 
 
 }
